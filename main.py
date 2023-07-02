ffb3487858f66d2c969eb4cba21378b2cecbf829
Uwe Strahlendorf <ustrahlendorf@tecracer.de>
2023-07-02 11:18:58 +0200

    2nd yaxis added

--- main.py
+++ main.py
@@ -1,34 +1,68 @@
 import taipy as tp
+import pandas as pd
 
 text = "Original text"
 col1 = "first col"
 col2 = "second col"
 col3 = "third col"
 
-page = """
+ballon_img = "./img/Ballon_15_20.png"
+
+section_1 = """
 <h1 align="center">Getting started with Taipy GUI</h1>
 
+<|layout|columns=1 2 2|
+<|
 My text: <|{text}|>
-
 <|{text}|input|>
-<br/>
-
-<center>
-<|Press Me|button|on_action=on_button_action|>
-</center>
-
-
-<|layout|gap=50px|columns=1fr 2fr 1fr|
-<|{text}|>
-<|{col2}|>
-<|{col3}|>
 |>
 
+<|
 <center>
-![Mein Ballon](./img/Ballon_15_20.png)
+<|Press Me|button|on_action=on_button_action|>  
+**Ein Button:** <|{col1}|>
 </center>
+|>
 
+<|
+<center>
+<|{ballon_img}|image|height=30%|width=30%|label=This is one ballon|>
+</center>
+|>
+
+|>
 """
+
+section_2 = '''
+
+##Darstellung Gas-Verbrauch
+<|{dataset}|chart|mode=line|x=Datum|y[1]=Verbrauch|y[2]=Betriebsstunden|yaxis[2]=y2|layout={layout}|color[1]=green|color[2]=blue|>
+
+'''
+layout = {
+    "xaxis": {
+        # Force the title of the x axis
+        "title": "Time-Range"
+    },
+    "yaxis": {
+        # Force the title of the first y axis
+        "title": "Verbrauch",
+        # Place the first axis on the left
+        "side": "left"
+    },
+    "yaxis2": {
+        # Second axis overlays with the first y axis
+        "overlaying": "y",
+        # Place the second axis on the right
+        "side": "right",
+        # and give it a title
+        "title": "Betriebsstunden"
+    },
+    "legend": {
+        # Place the legend above chart
+        "yanchor": "middle"
+    }
+}
 
 def on_button_action(state):
     tp.gui.notify(state, 'info', f'The text is: {state.text}')
@@ -38,12 +72,18 @@ def on_change(state, var_name, var_value):
     if var_name == "text" and var_value == "Reset":
         state.text = ""
         return
+    
+def get_data(path: str):
+    dataset = pd.read_csv(path)
+    dataset["Datum"] = pd.to_datetime(dataset["Datum"], dayfirst=True).dt.date
+    return dataset
 
-gui = tp.Gui(page)
+gui = tp.Gui(page=section_1 + section_2)
+dataset = get_data("./dataset.csv")
 
 if __name__ == '__main__':
     # Execute by the _Python_ interpretor, for debug only.
-    tp.run(gui, title="Taipy Demo", use_reloader=True, dark_mode=True, port=5001, flask_log=True)
+    tp.run(gui, title="Taipy Demo", use_reloader=True, dark_mode=True, port=5001, flask_log=False)
 else:
     # Execute by _Gunicorn_, for production environment.
     app = tp.run(gui, title="Taipy Demo", run_server=False)
